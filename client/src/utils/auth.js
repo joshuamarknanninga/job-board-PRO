3d2afe4d9eaab81485bbb800d9edfcad7addbc47
Joshua Nanninga <jmarknanninga@gmail.com>
2024-10-09 21:42:31 -0500

    added middleware

---
+++ client/src/utils/auth.js
@@ -0,0 +1,13 @@
+// auth.js
+export const setToken = (token) => {
+    localStorage.setItem('token', token);
+  };
+  
+  export const getToken = () => {
+    return localStorage.getItem('token');
+  };
+  
+  export const logout = () => {
+    localStorage.removeItem('token');
+  };
+  
